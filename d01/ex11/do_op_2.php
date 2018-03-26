246e6da3c114538a7787b56981a68670fd0d0112
Jeremy JAUZION <jjauzion@e3r11p23.42.fr>
2018-03-26 19:34:50 +0200

    ex11

---
+++ d01/ex11/do_op_2.php
@@ -0,0 +1,85 @@
+#!/usr/bin/php
+<?PHP
+
+if ($argc != 2)
+{
+	echo "Incorrect Parameters\n";
+	exit;
+}
+$tab = preg_split('/([+]|\*|\%|\/)/m', $argv[1], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+
+if (count($tab) == 1)
+{
+	$tab = preg_split('/(-)/', $argv[1], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+	$count = count($tab);
+	$i = -1;
+	while (++$i < $count)
+		$tab[$i] = trim($tab[$i]);
+	$i = -1;
+	$v1 = FALSE;
+	while ($i < $count && !is_numeric($tab[$i]))
+		$i++;
+	if ($i >= $count)
+	{
+		echo "Syntax Error\n";
+		exit;
+	}
+	else
+	{
+		$sign = 1;
+		if ($i > 0 && $tab[$i-1] == "-")
+			$sign = -1;
+		$v1 = $tab[$i] * $sign;
+	}
+	$i++;
+	while ($i < $count && $tab[$i] != "-")
+		$i++;
+	if ($i + 1 >= $count)
+	{
+		echo "Syntax Error\n";
+		exit;
+	}
+	else if (!is_numeric($tab[$i + 1]))
+		$opp = "-";
+	else
+		$opp = "+";
+	$i++;
+	while ($i < $count && !is_numeric($tab[$i]))
+		$i++;
+	if ($i + 1 != $count)
+	{
+		echo "Syntax Error\n";
+		exit;
+	}
+	else
+	{
+		$sign = 1;
+		if ($i > 0 && $tab[$i-1] == "-")
+			$sign = -1;
+		$v2 = $tab[$i] * $sign;
+	}
+}
+else
+{
+	$v1 = trim($tab[0]);
+	$opp = trim($tab[1]);
+	$v2 = trim($tab[2]);
+	if (count($tab) != 3 || !is_numeric($v1) || !is_numeric($v2))
+	{
+		echo "Syntax Error\n";
+		exit;
+	}
+}
+
+if ($opp == "+")
+	echo $v1 + $v2."\n";
+else if ($opp == "-")
+	echo $v1 - $v2."\n";
+else if ($opp == "*")
+	echo $v1 * $v2."\n";
+else if ($opp == "/")
+	echo $v1 / $v2."\n";
+else if ($opp == "%")
+	echo $v1 % $v2."\n";
+
+?>
