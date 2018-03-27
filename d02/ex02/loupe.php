2fa818bcb364fde248bf905e17081e50db9b8764
Jeremy Jauzion <jjauzion@Jeremys-MacBook-Pro.local>
2018-03-27 23:11:17 +0200

    ex2

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -1,12 +1,21 @@
 #!/usr/bin/php
 <?PHP
 
+function handle_lvl2($matches)
+{
+	return strtoupper($matches[0]);
+}
+
+function handle_lvl1($matches)
+{
+	$str = preg_replace_callback('/(?<=title=").*"|(?<=>).*(?=<)/sUi', "handle_lvl2", $matches[0]);
+	return $str;
+}
+
 if ($argc <= 1)
 	exit;
-
 if (($str = file_get_contents($argv[1])) === FALSE)
 	exit;
-
-$str = 
-
+$str = preg_replace_callback('/(?<=<a href).*(?=\/a>)/sUi', "handle_lvl1", $str);
+echo "$str";
 ?>
