16f5068d44aac9f1d210b0109b23edc787a69e49
Jeremy JAUZION <jjauzion@e2r3p21.42.fr>
2018-03-28 15:59:33 +0200

    correction d01ex07

--- d01/ex07/rostring.php
+++ d01/ex07/rostring.php
@@ -6,6 +6,11 @@ if ($argc < 2)
 	exit;
 }
 $tab = preg_split("/ /", $argv[1], -1, PREG_SPLIT_NO_EMPTY);
+if (count($tab) <= 1)
+{
+	echo "$tab[0]\n";
+	exit;
+}
 $output = $tab[1];
 foreach ($tab as $key => $elem)
 {
