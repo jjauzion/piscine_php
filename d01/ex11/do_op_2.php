f18375d441842430216f0bd2634e6b9f9ad696a0
Jeremy JAUZION <jjauzion@e2r3p21.42.fr>
2018-03-28 21:16:47 +0200

    modif doop2

--- d01/ex11/do_op_2.php
+++ d01/ex11/do_op_2.php
@@ -10,7 +10,7 @@ $tab = preg_split('/([+]|\*|\%|\/)/m', $argv[1], -1, PREG_SPLIT_DELIM_CAPTURE | 
 
 if (count($tab) == 1)
 {
-	$tab = preg_split('/(-)/', $argv[1], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+	$tab = preg_split('/( *- *)/', $argv[1], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
 	$count = count($tab);
 	$i = -1;
 	while (++$i < $count)
@@ -19,7 +19,7 @@ if (count($tab) == 1)
 	$v1 = FALSE;
 	while ($i < $count && !is_numeric($tab[$i]))
 		$i++;
-	if ($i >= $count)
+	if ($i > 1)
 	{
 		echo "Syntax Error\n";
 		exit;
@@ -32,32 +32,32 @@ if (count($tab) == 1)
 		$v1 = $tab[$i] * $sign;
 	}
 	$i++;
-	while ($i < $count && $tab[$i] != "-")
-		$i++;
-	if ($i + 1 >= $count)
+	if ($tab[$i] != '-')
 	{
 		echo "Syntax Error\n";
 		exit;
 	}
-	else if (!is_numeric($tab[$i + 1]))
-		$opp = "-";
-	else
-		$opp = "+";
 	$i++;
-	while ($i < $count && !is_numeric($tab[$i]))
+	if ($tab[$i] == '-')
+	{
+		if ($i + 2 != $count || !is_numeric($tab[$i+1]))
+		{
+			echo "Syntax Error\n";
+			exit;
+		}
 		$i++;
-	if ($i + 1 != $count)
+		$v2 = $tab[$i] * -1;
+	}
+	else if (is_numeric($tab[$i]) && $i + 1 == $count)
+	{
+		$v2 = $tab[$i];
+	}
+	else
 	{
 		echo "Syntax Error\n";
 		exit;
 	}
-	else
-	{
-		$sign = 1;
-		if ($i > 0 && $tab[$i-1] == "-")
-			$sign = -1;
-		$v2 = $tab[$i] * $sign;
-	}
+	$opp = "-";
 }
 else
 {
@@ -78,7 +78,14 @@ else if ($opp == "-")
 else if ($opp == "*")
 	echo $v1 * $v2."\n";
 else if ($opp == "/")
+{
+	if ($v2 == 0)
+	{
+		echo "Divsion par 0 interdite\n";
+		exit;
+	}
 	echo $v1 / $v2."\n";
+}
 else if ($opp == "%")
 	echo $v1 % $v2."\n";
 
--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -1,6 +1,11 @@
 #!/usr/bin/php
 <?PHP
 
-$c = curl_init("
+$c = curl_init("http://www.42.fr");
+$str = curl_exec($c);
+
+preg_match('/(?<=<img).*(?<=src=")(.*)(?=").*(?=>)/', $str, $matches);
+
+print_r($matches[1]);
 
 ?>
